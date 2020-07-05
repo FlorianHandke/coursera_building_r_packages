2cc730ecc21fef2f65fcfd4f971fbc94bb2e0e10
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2020-07-05 22:42:19 +0200

    changed tests

--- tests/testthat/test-tests.R
+++ tests/testthat/test-tests.R
@@ -2,5 +2,5 @@ library(testthat)
 library(mapdata)
 library(farsdata)
 test_that('Test that mapping works', {
-  expect_that(fars_map_state(44, 2016), is_null())
+  expect_error(fars_map_state(44, 2016))
 })
